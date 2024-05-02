a09151108705988a25fe211e5939745a7c1af8c0
Krzysztof Kortas <krzysztofkortas96@gmail.com>
2024-05-02 21:46:15 +0200

    Solve day1 part2
    This solution is very slow - it takes 13 minutes to compile.

--- aoc2023/day01.cpp
+++ aoc2023/day01.cpp
@@ -28,8 +28,33 @@ constexpr int calculatePart1(std::string_view input) {
       0, std::plus<>{});
 }
 
+constexpr void replace(std::string &str, std::string_view pattern,
+                       std::string_view replacement) {
+  auto pos = str.find(pattern);
+  while (pos != std::string::npos) {
+    str.replace(pos, pattern.length(), replacement);
+    pos = str.find(pattern, pos + replacement.length());
+  }
+}
+
+constexpr int calculatePart2(std::string_view input) {
+  std::string s{input};
+  replace(s, "one", "one1one");
+  replace(s, "two", "two2two");
+  replace(s, "three", "three3three");
+  replace(s, "four", "four4four");
+  replace(s, "five", "five5five");
+  replace(s, "six", "six6six");
+  replace(s, "seven", "seven7seven");
+  replace(s, "eight", "eight8eight");
+  replace(s, "nine", "nine9nine");
+  return calculatePart1(s);
+}
+
 static_assert(calculatePart1(day01::sample_part1) == 142);
+static_assert(calculatePart2(day01::sample_part2) == 281);
 
 static_assert(calculatePart1(day01::input) == 54601);
+static_assert(calculatePart2(day01::input) == 54078);
 
 } // anonymous namespace
