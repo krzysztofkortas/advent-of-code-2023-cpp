a6721085ce22c0ed2b05787e8f800b4a82db3eee
Krzysztof Kortas <krzysztofkortas96@gmail.com>
2024-08-03 20:15:49 +0200

    Refactor day 21

--- src/day21.cpp
+++ src/day21.cpp
@@ -16,134 +16,126 @@
 namespace
 {
 
-namespace vw = std::ranges::views;
+namespace rng = std::ranges;
+namespace vw = std::views;
 
 using std::int64_t;
 
 using Grid = std::vector<std::string>;
 using Position = std::pair<int64_t, int64_t>;
+using Positions = std::set<Position>;
 using Direction = std::pair<int64_t, int64_t>;
 
-Position getStartPosition(const Grid& grid)
-{
-	for (const auto& [rowIndex, row] : grid | vw::enumerate)
-		for (const auto& [colIndex, c] : row | vw::enumerate)
-			if (c == 'S')
-				return {rowIndex, colIndex};
-
-	std::unreachable();
-}
-
-int64_t solvePart1(std::string_view input, int64_t steps)
-{
-	const Grid grid = input | vw::split('\n') | std::ranges::to<Grid>();
-	const int64_t height = std::ssize(grid);
-	const int64_t width = std::ssize(grid.front());
-
-	const Position start = getStartPosition(grid);
-	std::set<Position> plots{start};
-
-	for (int i = 0; i < steps; ++i)
-	{
-		std::set<Position> newPlots;
-		for (const auto& plot : plots)
-		{
-			for (const auto& dir : {Direction{-1, 0}, {1, 0}, {0, -1}, {0, 1}})
-			{
-				const Position newPlot{plot.first + dir.first, plot.second + dir.second};
-				if (std::min(newPlot.first, newPlot.second) < 0 || newPlot.first > height
-					|| newPlot.second > width)
-				{
-					continue;
-				}
-				const char c = grid.at(newPlot.first).at(newPlot.second);
-				if (c == '.' || c == 'S')
-					newPlots.insert(newPlot);
-			}
-		}
-		plots = newPlots;
-	}
-
-	return std::ssize(plots);
-}
+constexpr auto directions = {Direction{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
 int64_t getEuclideanRemainder(int64_t val, int64_t modulo)
 {
 	return (val % modulo + modulo) % modulo;
 }
 
-int64_t solvePart2(std::string_view input, int64_t steps)
+class PlotsCounter
 {
-	const Grid grid = input | vw::split('\n') | std::ranges::to<Grid>();
-	const int64_t height = std::ssize(grid);
-	const int64_t width = std::ssize(grid.front());
-	assert(height == width);
-
-	const Position start = getStartPosition(grid);
-	std::set<Position> visited{start};
-	std::set<Position> plots{start};
-	int64_t distance = 0;
-	std::vector<int64_t> answers(steps + 1, 0);
-	answers[0] = 1;
-
-	const int64_t modulo = steps % width;
-	const int64_t precalculatedSteps = std::min(steps, (modulo + (3 * width)));
-
-	while (distance < precalculatedSteps)
+public:
+	explicit PlotsCounter(Grid grid)
+		: grid_{std::move(grid)}
+		, gridSize_{ssize(grid_)}
 	{
-		std::set<Position> newPlots;
-		for (const auto& plot : plots)
+	}
+
+	[[nodiscard]] int64_t countPlots(int64_t steps) const
+	{
+		const int64_t modulo = steps % gridSize_;
+		const int64_t precalculatedSteps = std::min(steps, (modulo + (2 * gridSize_)));
+		const std::vector<int64_t> answers = getPrecalculatedAnswers(precalculatedSteps);
+
+		if (steps <= precalculatedSteps)
+			return answers[steps];
+
+		return extrapolate(
+			{answers[modulo], answers[modulo + gridSize_], answers[modulo + (2 * gridSize_)]},
+			steps);
+	}
+
+private:
+	[[nodiscard]] Position getStartingPosition() const
+	{
+		for (const auto& [rowIndex, row] : grid_ | vw::enumerate)
+			for (const auto& [colIndex, c] : row | vw::enumerate)
+				if (c == 'S')
+					return {rowIndex, colIndex};
+
+		std::unreachable();
+	}
+
+	[[nodiscard]] char getTile(const Position& pos) const
+	{
+		return grid_.at(getEuclideanRemainder(pos.first, gridSize_))
+			.at(getEuclideanRemainder(pos.second, gridSize_));
+	}
+
+	[[nodiscard]] std::vector<int64_t> getPrecalculatedAnswers(int64_t precalculatedSteps) const
+	{
+		const Position start = getStartingPosition();
+		Positions visited{start};
+		Positions plots{start};
+		std::vector<int64_t> answers(precalculatedSteps + 1, 0);
+		answers[0] = 1;
+
+		for (int64_t distance : vw::iota(1, precalculatedSteps + 1))
 		{
-			for (const auto& dir : {Direction{-1, 0}, {1, 0}, {0, -1}, {0, 1}})
-			{
-				const Position newPlot{plot.first + dir.first, plot.second + dir.second};
-				if (visited.contains(newPlot))
-					continue;
-				const char c = grid.at(getEuclideanRemainder(newPlot.first, height))
-								   .at(getEuclideanRemainder(newPlot.second, width));
-				if (c == '.' || c == 'S')
-				{
-					newPlots.insert(newPlot);
-					visited.insert(newPlot);
-				}
-			}
+			plots = getNewPlots(plots, visited);
+			visited.insert(plots.cbegin(), plots.cend());
+
+			answers[distance] = ssize(plots);
+			if (distance >= 2)
+				answers[distance] += answers[distance - 2];
 		}
-		plots = newPlots;
-		++distance;
-		answers[distance] = std::ssize(plots);
-		if (distance >= 2)
-			answers[distance] += answers[distance - 2];
+
+		return answers;
 	}
 
-	if (steps <= precalculatedSteps)
-		return answers[steps];
-
-	answers = {answers[modulo], answers[modulo + width], answers[modulo + (2 * width)]};
-
-	int64_t answersSize = 3;
-	const int64_t neededSteps = (steps + width - 1) / width;
-	const int64_t diff = answers[2] - (2 * answers[1]) + answers[0];
-
-	while (answersSize < neededSteps)
+	[[nodiscard]] Positions getNewPlots(const Positions& plots, const Positions& visited) const
 	{
-		answers.push_back(diff + (2 * answers[answersSize - 1]) - answers[answersSize - 2]);
-		++answersSize;
+		return vw::cartesian_product(plots, directions) | vw::transform([&](const auto& p) {
+			const auto& [plot, dir] = p;
+			return Position{plot.first + dir.first, plot.second + dir.second};
+		}) | vw::filter([&](const Position& plot) {
+			return !visited.contains(plot) && getTile(plot) != '#';
+		}) | rng::to<Positions>();
 	}
 
-	return answers.back();
+	[[nodiscard]] int64_t extrapolate(std::vector<int64_t> answers, int64_t steps) const
+	{
+		const int64_t neededSteps = (steps + gridSize_ - 1) / gridSize_;
+		const int64_t diff = answers.at(2) - (2 * answers.at(1)) + answers.at(0);
+
+		for (int64_t answersSize : vw::iota(ssize(answers), neededSteps))
+			answers.push_back(diff + (2 * answers[answersSize - 1]) - answers[answersSize - 2]);
+
+		return answers.back();
+	}
+
+	Grid grid_;
+	int64_t gridSize_;
+};
+
+int64_t solve(std::string_view input, int64_t steps)
+{
+	const Grid grid = input | vw::split('\n') | rng::to<Grid>();
+	return PlotsCounter{grid}.countPlots(steps);
 }
 
 TEST(day21, test)
 {
-	EXPECT_EQ(solvePart1(day21::sample, 1), 2);
-	EXPECT_EQ(solvePart1(day21::sample, 2), 4);
-	EXPECT_EQ(solvePart1(day21::sample, 3), 6);
-	EXPECT_EQ(solvePart1(day21::sample, 6), 16);
-	EXPECT_EQ(solvePart1(day21::input, 64), 3762);
+	EXPECT_EQ(solve(day21::sample, 1), 2);
+	EXPECT_EQ(solve(day21::sample, 2), 4);
+	EXPECT_EQ(solve(day21::sample, 3), 6);
+	EXPECT_EQ(solve(day21::sample, 6), 16);
+	EXPECT_EQ(solve(day21::input, 64), 3762);
 
-	EXPECT_EQ(solvePart2(day21::sample, 6), 16);
-	EXPECT_EQ(solvePart2(day21::sample, 10), 50);
-	EXPECT_EQ(solvePart2(day21::input, 26501365), 621944727930768);
+	EXPECT_EQ(solve(day21::sample, 6), 16);
+	EXPECT_EQ(solve(day21::sample, 10), 50);
+	EXPECT_EQ(solve(day21::input, 26501365), 621944727930768);
 }
 
 } // anonymous namespace
