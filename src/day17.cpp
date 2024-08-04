b2fb047bc4b6a638385ff02c0f556ccabda4123f
Krzysztof Kortas <krzysztofkortas96@gmail.com>
2024-08-04 13:46:01 +0200

    Refactor day 17

--- src/day17.cpp
+++ src/day17.cpp
@@ -7,7 +7,6 @@
 #include <ranges>
 #include <string>
 #include <string_view>
-#include <unordered_map>
 #include <utility>
 #include <vector>
 
@@ -16,7 +15,8 @@
 namespace
 {
 
-namespace vw = std::ranges::views;
+namespace rng = std::ranges;
+namespace vw = std::views;
 
 using std::int64_t;
 
@@ -25,46 +25,36 @@ using Direction = std::pair<int64_t, int64_t>;
 using Position = std::pair<int64_t, int64_t>;
 using Key = std::pair<Position, Direction>;
 
-bool isValid(const Position& pos, int64_t width, int64_t height)
-{
-	return pos.first >= 0 && pos.first < height && pos.second >= 0 && pos.second < width;
-}
+constexpr auto directions = {Direction{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-int64_t getHeatLoss(const Grid& grid, int64_t minDistance, int64_t maxDistance)
+class HeatLossSolver
 {
-	const int64_t height = std::ssize(grid);
-	const int64_t width = std::ssize(grid.at(0));
-	std::map<Key, int64_t> distMap;
-	using QueueKey = std::pair<int64_t, Key>;
-	std::priority_queue<QueueKey, std::vector<QueueKey>, std::greater<>> pq;
-	pq.push({0, {{0, 0}, {0, 0}}});
-
-	while (!pq.empty())
+public:
+	HeatLossSolver(Grid grid, int64_t minConsecutive, int64_t maxConsecutive)
+		: grid_{std::move(grid)}
+		, minConsecutive_{minConsecutive}
+		, maxConsecutive_{maxConsecutive}
+		, width_{ssize(grid_.at(0))}
+		, height_{ssize(grid_)}
 	{
-		const auto [cost, key] = pq.top();
-		pq.pop();
-		const auto [pos, dir] = key;
-		if (pos == Position{height - 1, width - 1})
-			return cost;
+	}
 
-		for (const auto& d : {Direction{-1, 0}, {1, 0}, {0, -1}, {0, 1}})
+	[[nodiscard]] int64_t getHeatLoss() const
+	{
+		const Position destination{height_ - 1, width_ - 1};
+		std::map<Key, int64_t> distMap;
+		std::priority_queue<QueueKey, std::vector<QueueKey>, std::greater<>> pq;
+		pq.push({0, {{0, 0}, {0, 0}}});
+
+		for (; !pq.empty(); pq.pop())
 		{
-			if (dir == d || dir == Direction{-d.first, -d.second})
-				continue;
-			int64_t nextCost = cost;
+			const auto [cost, key] = pq.top();
+			const auto& [pos, dir] = key;
+			if (pos == destination)
+				return cost;
 
-			for (const int64_t distance : vw::iota(1, maxDistance + 1))
+			for (const auto [nextCost, nextKey] : getNextPositions(key, cost))
 			{
-				const Position nextPos{
-					pos.first + (distance * d.first), pos.second + (distance * d.second)};
-				if (!isValid(nextPos, width, height))
-					continue;
-
-				nextCost += grid.at(nextPos.first).at(nextPos.second);
-				if (distance < minDistance)
-					continue;
-
-				const Key nextKey{nextPos, d};
 				if (auto it = distMap.find(nextKey); it == distMap.end() || nextCost < it->second)
 				{
 					distMap.insert_or_assign(nextKey, nextCost);
@@ -72,32 +62,75 @@ int64_t getHeatLoss(const Grid& grid, int64_t minDistance, int64_t maxDistance)
 				}
 			}
 		}
+
+		std::unreachable();
 	}
 
-	std::unreachable();
-}
+private:
+	using QueueKey = std::pair<int64_t, Key>;
 
-int64_t solve(std::string_view input, int64_t minDistance, int64_t maxDistance)
+	[[nodiscard]] std::vector<QueueKey> getNextPositions(const Key& key, int64_t cost) const
+	{
+		const auto& [pos, dir] = key;
+		return directions | vw::filter([&dir](const Direction& d) {
+			return dir != d && dir != Direction{-d.first, -d.second};
+		}) | vw::transform([&](const Direction& d) { return getNextPositions(pos, d, cost); })
+			| vw::join | rng::to<std::vector>();
+	}
+
+	[[nodiscard]] std::vector<QueueKey> getNextPositions(
+		const Position& pos, const Direction& dir, int64_t cost) const
+	{
+		std::vector<QueueKey> positions;
+		for (int64_t nextCost = cost; const int64_t consecutive : vw::iota(1, maxConsecutive_ + 1))
+		{
+			const Position nextPos{
+				pos.first + (consecutive * dir.first), pos.second + (consecutive * dir.second)};
+
+			if (isValid(nextPos))
+			{
+				nextCost += grid_.at(nextPos.first).at(nextPos.second);
+				if (consecutive >= minConsecutive_)
+					positions.emplace_back(nextCost, Key{nextPos, dir});
+			}
+		}
+
+		return positions;
+	}
+
+	[[nodiscard]] bool isValid(const Position& pos) const
+	{
+		return pos.first >= 0 && pos.first < height_ && pos.second >= 0 && pos.second < width_;
+	}
+
+	Grid grid_;
+	int64_t minConsecutive_;
+	int64_t maxConsecutive_;
+	int64_t width_;
+	int64_t height_;
+};
+
+int64_t solve(std::string_view input, int64_t minConsecutive, int64_t maxConsecutive)
 {
 	const Grid grid = input | vw::split('\n') | vw::transform([](auto&& line) {
 		return line | vw::transform([](char c) { return static_cast<int64_t>(c - '0'); });
-	}) | std::ranges::to<Grid>();
+	}) | rng::to<Grid>();
 
-	return getHeatLoss(grid, minDistance, maxDistance);
+	return HeatLossSolver{grid, minConsecutive, maxConsecutive}.getHeatLoss();
 }
 
 int64_t solvePart1(std::string_view input)
 {
-	constexpr int minDistance = 1;
-	constexpr int maxDistance = 3;
-	return solve(input, minDistance, maxDistance);
+	constexpr int minConsecutive = 1;
+	constexpr int maxConsecutive = 3;
+	return solve(input, minConsecutive, maxConsecutive);
 }
 
 int64_t solvePart2(std::string_view input)
 {
-	constexpr int minDistance = 4;
-	constexpr int maxDistance = 10;
-	return solve(input, minDistance, maxDistance);
+	constexpr int minConsecutive = 4;
+	constexpr int maxConsecutive = 10;
+	return solve(input, minConsecutive, maxConsecutive);
 }
 
 TEST(day17, test)
