6d3f6e5fa0090560ba5cc92f0d51a585a0dd8df1
Krzysztof Kortas <krzysztofkortas96@gmail.com>
2026-02-07 14:35:29 +0100

    Update clang and gcc compilers

--- vcpkg/toolchains/clang-21-toolchain.cmake
+++
@@ -1,4 +0,0 @@
-set(CMAKE_C_COMPILER "/usr/bin/clang-21")
-set(CMAKE_CXX_COMPILER "/usr/bin/clang++-21")
-set(CMAKE_CXX_FLAGS
-    "--gcc-install-dir=/usr/local/lib/gcc/x86_64-linux-gnu/16.0.0 --stdlib=libstdc++")
---
+++ vcpkg/toolchains/clang-toolchain.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_C_COMPILER "/usr/bin/clang-23")
+set(CMAKE_CXX_COMPILER "/usr/bin/clang++-23")
+set(CMAKE_CXX_FLAGS
+    "--gcc-install-dir=/usr/local/lib/gcc/x86_64-linux-gnu/16.0.1 --stdlib=libstdc++")
